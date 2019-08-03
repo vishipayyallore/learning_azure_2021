52cf4fbb00719355b792eb6c9bde120f51941332
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-08-03 20:43:37 +0530

    Added Students Controller

--- CosmosDbSamples/FirstSample30/Controllers/StudentsController.cs
+++ CosmosDbSamples/FirstSample30/Controllers/StudentsController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstSample30.Entities;
+using FirstSample30.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace FirstSample30.Controllers
 {
@@ -11,5 +14,27 @@ namespace FirstSample30.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        private readonly CollegeDbContext _collegeDbContext;
+        private readonly ILogger<StudentsController> _logger;
+
+        public StudentsController(CollegeDbContext collegeDbContext, ILogger<StudentsController> logger)
+        {
+            _collegeDbContext = collegeDbContext;
+
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            var students = _collegeDbContext
+                                .Students
+                                .ToList();
+
+            return Ok(students);
+        }
+
     }
+
 }
