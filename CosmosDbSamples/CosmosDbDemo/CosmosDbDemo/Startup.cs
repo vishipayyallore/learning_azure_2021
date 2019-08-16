db360852b4a728ad40e98ff737b6b8f502e39dce
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-08-16 22:01:34 +0530

    changes

--- CosmosDbSamples/CosmosDbDemo/CosmosDbDemo/Startup.cs
+++ CosmosDbSamples/CosmosDbDemo/CosmosDbDemo/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
 
-namespace CosmosDbDemo
+namespace FirstSample30
 {
     public class Startup
     {
@@ -24,18 +18,57 @@ namespace CosmosDbDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
+
+            //services.AddDbContext<CollegeDbContext>(o => o.UseCosmos(
+            //    "https://localhost:8081/",
+            //    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            //    "webapidemodb",
+            //    optionBuilder =>
+            //    {
+            //        optionBuilder.ExecutionStrategy(d => new CosmosExecutionStrategy(d, 10));
+            //    }
+            //    ));
+
+            //services.AddDbContext<CollegeDbContext>(options =>
+            //    {
+            //            options.UseCosmos("https://localhost:8081/", 
+            //                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            //                "webapidemodb");
+            //    }, ServiceLifetime.Transient);
+
+            //services.AddDbContext<NoSqlDbContext>(options =>
+            //  options.UseCosmos(
+            //      //I use user secrets to provide the actual Azure Cosmos database, but fall back to local emulator if no secrets set
+            //      Configuration["CosmosUrl"] ?? Configuration["endpoint"],
+            //      Configuration["CosmosKey"] ?? Configuration["authKey"],
+            //      Configuration["database"]));
+
+            //services.AddDbContext<CollegeDbContext>(options =>
+            //    options.UseCosmos(
+            //        "https://localhost:8081/",
+            //        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            //        "webapidemodb"));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
