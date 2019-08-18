4a72268bfc9028e2e343eb0984476b2c104b0ebb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-08-18 21:44:51 +0530

    Added DB Context

---
+++ CosmosDbSamples/CosmosDbDemo1/Persistence/NoSqlDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CosmosDbDemo1.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDbDemo1.Persistence
+{
+
+    public class NoSqlDbContext : DbContext
+    {
+        public NoSqlDbContext(DbContextOptions<NoSqlDbContext> options)
+            : base(options) { }
+
+        public DbSet<BookListNoSql> Books { get; set; }
+
+        //thanks to https://csharp.christiannagel.com/2018/09/05/efcorecosmos/
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //see this for timestamp https://docs.microsoft.com/en-us/azure/cosmos-db/working-with-dates#storing-datetimes
+            modelBuilder.Entity<BookListNoSql>().Property<long>("_ts").ValueGeneratedOnAddOrUpdate();
+        }
+    }
+
+}
