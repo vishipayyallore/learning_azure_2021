0f417e0022f08f4ebd32eea7029c8e9c9fd35b14
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-29 17:49:35 +0530

    Initial Version

--- 2020/AzureSamples.WinApp/AzureSamples.WinApp/Program.cs
+++ 2020/AzureSamples.WinApp/AzureSamples.WinApp/Program.cs
@@ -16,7 +16,7 @@ namespace AzureSamples.WinApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MDIParentAzureDemos());
         }
     }
 }
