bdaf415851ee15391178c3f3ef80b823d398e6b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-17 22:09:40 +0530

    Few Changes

--- 2020/firststoragedemo/Program.cs
+++ 2020/firststoragedemo/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace firststoragedemo
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration _config;
+
+        static async Task Main(string[] args)
         {
+            _config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .AddUserSecrets("62799a27-409a-4c9b-82d7-0d33627868f9")
+                            .Build();
+            
+            if (!CloudStorageAccount.TryParse(_config["StorageAccountConnectionString"], out CloudStorageAccount storageAccount))
+            {
+                Console.WriteLine("Unable to parse connection string");
+                return;
+            }
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("photoblobs");
+            bool created = await blobContainer.CreateIfNotExistsAsync();
+
+            Console.WriteLine(created ? "Created the Blob container" : "Blob container already exists.");
+
+            // Create a local object to represent our blob. No network call.
+            string blobName = "userPhoto";
+            CloudBlockBlob blob = blobContainer.GetBlockBlobReference(blobName);
+
+            // This transfers data in the file to the blob on the service.
+            string fileName = "./Images/first.png";
+            await blob.UploadFromFileAsync(fileName);
+
             Console.WriteLine("Hello World!");
-        }
+
+        } 
     }
 }
