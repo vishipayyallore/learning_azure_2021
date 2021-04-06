8e5018a189a6cfd37cb8ab2a179b8ea5bc617d2b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-06 23:55:20 +0530

    Update CreateVmUbuntu.sh

--- DevelopingInAzure/VirtualMachines/CreateVmUbuntu.sh
+++ DevelopingInAzure/VirtualMachines/CreateVmUbuntu.sh
@@ -20,6 +20,7 @@ az group create --name $RGName --location $LocationName
 
 ##### Virtual Machine
 az vm create --resource-group $RGName --name $VmName --image $ImageName --admin-username $username --authentication-type ssh --ssh-key-value C:\Users\PK.Viswanatha-Swamy\.ssh\id_rsa.pub
+ az vm create --resource-group $RGName --name $VmName --image $ImageName --admin-username $username --authentication-type ssh --ssh-key-value ~/.ssh/id_rsa.pub
 
 ##### Opending the ports
 az vm open-port --resource-group $RGName --name $VmName --port $PortToOpen --priority 900
@@ -29,6 +30,7 @@ az vm list-ip-addresses --resource-group $RGName --name $VmName --output table
 
 ##### Login into VM
 ssh demouser@PublicIpAddress
+ssh -i ~/.ssh/id_rsa demouser@13.82.31.14 #From Ubuntn VM (Private Key)
 
 ##### From within the Ubuntu VM
 sudo apt-get -y update
