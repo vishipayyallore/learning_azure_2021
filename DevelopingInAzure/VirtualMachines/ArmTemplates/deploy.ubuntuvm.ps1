4fe4d5e4227de421c1ba0496e42bb286dafcf577
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-06 12:37:35 +0530

    Few Changes

--- DevelopingInAzure/VirtualMachines/ArmTemplates/deploy.ubuntuvm.ps1
+++ DevelopingInAzure/VirtualMachines/ArmTemplates/deploy.ubuntuvm.ps1
@@ -1,5 +1,5 @@
 $SubscriptionName = "Visual Studio Professional Subscription"
-$RGName = "rg-az204-armpsubuntu-dev-001"
+$RGName = "rg-az204-arm-psubuntu-dev-001"
 $LocationName = "eastus"
 
 Connect-AzAccount
@@ -8,16 +8,20 @@ Set-AzContext -SubscriptionName $SubscriptionName
 #use this command when you need to create a new resource group for your deployment
 New-AzResourceGroup -Name $RGName -Location $LocationName 
 
+##### Please ensure that updated SSH keys are present in parameters.json
 ##### get-help New-AzResourceGroupDeployment -examples
-New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile "./ubuntuvm.deploy.json" -TemplateParameterFile "./ubuntuvm.parameters.json" -Tag @{"environment"="dev"; "contact"="NoName";}
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile "./ubuntuvm.deploy.json" -TemplateParameterFile "./ubuntuvm.parameters.json"
 
+##### Open the Port 80
 Get-AzNetworkSecurityGroup -Name "SecGroupNet" -ResourceGroupName $RGName | Add-AzNetworkSecurityRuleConfig -Name "Web-Rule" -Description "Allow Web" -Access "Allow" -Protocol "Tcp" -Direction "Inbound" -Priority 100 -SourceAddressPrefix "Internet" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "80" | Set-AzNetworkSecurityGroup
 
+Get-AzPublicIpAddress -ResourceGroupName $RGName | Select-Object IpAddress
+
 ##### From PowerShell
-> ssh -i C:\Users\PK.Viswanatha-Swamy\.ssh\id_rsa demouser@52.255.149.104
+> ssh -i C:\Users\PK.Viswanatha-Swamy\.ssh\id_rsa demouser@52.168.25.254
 
 ##### Inside the Ubuntu VM
 ##### sudo apt update && sudo apt install -y lamp-server^
 
 # visit the URL
-http://52.255.149.104
+http://52.168.25.254
---
+++ DevelopingInAzure/VirtualMachines/ArmTemplates/deploy.ubuntuvm.sh
@@ -0,0 +1,37 @@
+SubscriptionName="Visual Studio Professional Subscription"
+RGName="rg-az204-arm-azubuntu-dev-001"
+LocationName="EastUS"
+BaseName="vmapr2021"
+VmName="simpleLinuxVM2" 
+PortToOpen=80
+username="demouser"
+ImageName="UbuntuLTS" 
+
+ #use this command when you need to create a new resource group for your deployment
+az group create --name $RGName --location $LocationName
+az group list --output table
+
+##### Deploy the VM
+az deployment group create --resource-group $RGName --template-file ./ubuntuvm.deploy.json --parameters ./ubuntuvm.parameters.json
+
+##### Opending the ports
+az vm open-port --resource-group $RGName --name $VmName --port $PortToOpen --priority 900
+
+##### IP Addresses for Remote Access
+az vm list-ip-addresses --resource-group $RGName --name $VmName --output table
+
+ssh -i ./id_rsa demouser@13.82.90.190
+
+##### From within the Ubuntu VM
+
+##### DEMO 1
+sudo apt update && sudo apt install -y lamp-server^
+logout
+
+##### DEMO 2
+sudo apt-get -y update
+sudo apt-get -y install nginx
+logout
+
+##### View the site.
+http://13.82.90.190
