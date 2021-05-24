c91d403a78247733539dc72bef4089b550917e8b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-24 22:10:26 +0530

    Orders getting placed in Cosmos Db

--- DevelopingInAzure/AzFunctions/ABCHospitalOrderingService/Source/OrderService/SendOrderToPharmacy.cs
+++ DevelopingInAzure/AzFunctions/ABCHospitalOrderingService/Source/OrderService/SendOrderToPharmacy.cs
@@ -1,16 +1,27 @@
-using System;
+using MedsRUs.Data;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace OrderService
 {
+
     public static class SendOrderToPharmacy
     {
+
         [FunctionName("SendOrderToPharmacy")]
-        public static void Run([ServiceBusTrigger("medicialsupplyorders", Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
+        public static async void Run([ServiceBusTrigger("medicialsupplyorders", Connection = "ServiceBusConnection")] string medicineOrderQueueItem,
+            [CosmosDB(databaseName: "MedsRUsDataStore", collectionName: "MedOrders", ConnectionStringSetting = "CosmosDBConnection")]
+            IAsyncCollector<MedicineOrder> medicineOrderAsyncCollector,
+            ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {medicineOrderQueueItem}");
+
+            MedicineOrder medicineOrder = JsonConvert.DeserializeObject<MedicineOrder>(medicineOrderQueueItem);
+
+            await medicineOrderAsyncCollector.AddAsync(medicineOrder);
         }
+
     }
+
 }
